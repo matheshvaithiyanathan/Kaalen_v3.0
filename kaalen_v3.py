f94b2cd3b6baf37dcc6be54f7a246a8b1c74d29b
Mathesh Vaithiyanathan <122552911+matheshvaithiyanathan@users.noreply.github.com>
2026-05-02 15:14:27 +0200

    Change window text color to black and add highlight colors

--- kaalen_v3.py
+++ kaalen_v3.py
@@ -2926,10 +2926,12 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     app.setStyle('Fusion') 
     palette = app.palette()
-    palette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.gray)  # Text on window/labels
+    palette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.black)  # Text on window/labels
     palette.setColor(QPalette.ColorRole.Text, Qt.GlobalColor.black)        # Text inside input boxes
     palette.setColor(QPalette.ColorRole.ButtonText, Qt.GlobalColor.black)  # Text on buttons
     palette.setColor(QPalette.ColorRole.Base, Qt.GlobalColor.white)        # Background for inputs
+    palette.setColor(QPalette.ColorRole.HighlightedText, Qt.GlobalColor.blue)
+    palette.setColor(QPalette.ColorRole.Highlight, Qt.GlobalColor.lightGray)
     app.setPalette(palette)
     app.setWindowIcon(QIcon(':/icons/icon.ico'))
     window = SignalPlotterApp()
